3acdfdeaf08b4282d979a1492e269689d286fabe
Abdieeel03 <marcelopostigo54@gmail.com>
2026-06-06 15:45:57 -0500

    Se configuro el manejo del contenedor

--- app/clients/PythonClient.scala
+++ app/clients/PythonClient.scala
@@ -1,18 +1,20 @@
 package clients
 
 import javax.inject._
+import play.api.Configuration
 import play.api.libs.json._
 import play.api.libs.ws._
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class PythonClient @Inject()(
-                              ws: WSClient
+                              ws: WSClient,
+                              config: Configuration
                             )(implicit ec: ExecutionContext){
-  private val pythonUrl = "http://localhost:8000/chat"
+  private val pythonUrl : String = config.get[String]("python.engine.url")
 
   def sendMessage(message: String): Future[JsValue] = {
-    ws.url(pythonUrl)
+    ws.url(s"$pythonUrl/chat")
       .post(
         Json.obj(
           "message" -> message
